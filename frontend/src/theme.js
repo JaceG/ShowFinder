33930389345431381ca0f6aed029a17ca4fc6300
Jace Galloway <jace.galloway@gmail.com>
2025-02-11 20:29:03 -0500

    fixed font permission errors for render

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material';
 export const getTheme = (mode) =>
 	createTheme({
 		typography: {
-			fontFamily: ['Mulish', 'Arial', 'sans-serif'].join(','),
+			fontFamily: 'system-ui, -apple-system, sans-serif',
 		},
 		palette: {
 			mode,
@@ -45,31 +45,6 @@ export const getTheme = (mode) =>
 				  }),
 		},
 		components: {
-			MuiCssBaseline: {
-				styleOverrides: `
-					@font-face {
-						font-family: 'Mulish';
-						font-style: normal;
-						font-display: swap;
-						font-weight: 400;
-						src: local('Mulish'), local('Mulish-Regular'), url('/fonts/Mulish-Regular.woff2') format('woff2');
-					}
-					@font-face {
-						font-family: 'Mulish';
-						font-style: normal;
-						font-display: swap;
-						font-weight: 700;
-						src: local('Mulish'), local('Mulish-Bold'), url('/fonts/Mulish-Bold.woff2') format('woff2');
-					}
-					@font-face {
-						font-family: 'Mulish';
-						font-style: normal;
-						font-display: swap;
-						font-weight: 300;
-						src: local('Mulish'), local('Mulish-Light'), url('/fonts/Mulish-Light.woff2') format('woff2');
-					}
-				`,
-			},
 			MuiCard: {
 				styleOverrides: {
 					root: ({ theme }) => ({
