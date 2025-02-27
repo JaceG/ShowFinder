8629ae2a6c171ade32749592df99275a21cf4a76
Jace Galloway <jace.galloway@gmail.com>
2025-02-27 12:21:21 -0500

    added hotjar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import { CssBaseline, StyledEngineProvider } from '@mui/material';
 import { ThemeProvider } from './context/ThemeContext';
+import Hotjar from '@hotjar/browser';
+
+// Initialize Hotjar
+const siteId = 5320792;
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
